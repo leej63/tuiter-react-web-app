7654648bc13d9e56ef05c6a5fb4ac339b5d7eb1b
Jonathan Lee <jonathanlee@Jonathans-MacBook-Air.local>
2023-07-21 02:34:24 -0700

    a3 lab sections 2.3 to 2.7

--- src/App.js
+++ src/App.js
@@ -2,14 +2,23 @@ import './App.css';
 import Labs from './labs';
 import HelloWorld from "./labs/a3/hello-world";
 import Tuiter from "./tuiter";
+import {HashRouter} from "react-router-dom";
+import {Routes, Route, Navigate} from "react-router";
+import {Link} from "react-router-dom";
 
 function App() {
   return (
-    <div className="container">
-      <HelloWorld />
-      <Labs />
-      <Tuiter />
-    </div>
+    <HashRouter>
+      {/* <Link to="hello">Hello World</Link> | <Link to="labs">Labs</Link> | <Link to="tuiter">Tuiter</Link> */}
+      <div className="container">
+        <Routes>
+            <Route path="/"         element={<Navigate to="/labs"/>}/>
+            <Route path="/labs/*"   element={<Labs/>}/>
+            <Route path="/hello"    element={<HelloWorld/>}/>
+            <Route path="/tuiter/*" element={<Tuiter/>}/>
+        </Routes>
+      </div>
+    </HashRouter>
   );
 }
 
--- src/labs/index.js
+++ src/labs/index.js
@@ -1,9 +1,17 @@
 import Assignment3 from "./a3";
+import {Routes, Route, Navigate} from "react-router";
+import {Link} from "react-router-dom";
+import Nav from "../nav";
 
 function Labs() {
    return(
       <div>
-         <Assignment3 />
+         {/* <Link to="/labs/a3">A3</Link> | <Link to="/hello">Hello</Link> | <Link to="/tuiter">Tuiter</Link> */}
+         <Nav/>
+         <Routes>
+            <Route path="/" element={<Navigate to="a3"/>}/>
+            <Route path="a3" element={<Assignment3/>}/>
+         </Routes>
       </div>
    );
 }
