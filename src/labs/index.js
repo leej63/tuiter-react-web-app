e9212e7ebdf9c921b992f04a6ae141419e10cafa
Jonathan Lee <jonathanlee@Jonathans-MacBook-Air.local>
2023-07-26 02:27:42 -0700

    a4, complete lab 2.1 to 2.9

--- src/labs/index.js
+++ src/labs/index.js
@@ -1,4 +1,5 @@
 import Assignment3 from "./a3";
+import Assignment4 from "./a4";
 import {Routes, Route, Navigate} from "react-router";
 import {Link} from "react-router-dom";
 import Nav from "../nav";
@@ -6,11 +7,11 @@ import Nav from "../nav";
 function Labs() {
    return(
       <div>
-         {/* <Link to="/labs/a3">A3</Link> | <Link to="/hello">Hello</Link> | <Link to="/tuiter">Tuiter</Link> */}
          <Nav/>
          <Routes>
             <Route path="/" element={<Navigate to="a3"/>}/>
             <Route path="a3" element={<Assignment3/>}/>
+            <Route path="a4" element={<Assignment4/>}/>
          </Routes>
       </div>
    );
